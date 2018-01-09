d238773c31d8044730088ee35268afbc83b1947c
Antoine LOIZEAU <antoine.loizeau@hotmail.fr>
2018-01-09 17:30:44 +0100

    Push first sample

--- markdowns/Collections.md
+++ markdowns/Collections.md
@@ -6,6 +6,7 @@ This C# template lets you get started quickly with a simple working example usin
 The source code is on [GitHub](https://github.com/TechDotIO/csharp-template), please feel free to come up with proposals to improve it.
 
 # Hands-on Demo
+@[Luke, how many stars are there in these galaxies?]({"stubs": ["Exercises/EqualStub.cs"],"command": ""})
 
 @[Luke, how many stars are there in these galaxies?]({"stubs": ["Exercises/UniverseStub.cs"],"command": "TechIo.UniverseTest.VerifyCountAllStars"})
 
--- src/Collections/Exercises/UniverseStub.cs
+++ src/Collections/Exercises/UniverseStub.cs
@@ -1,20 +1,22 @@
 // { autofold
-﻿using System;
+using System;
 using System.Linq;
 
 namespace Answer
 {
-	public class UniverseStub
-	{
-// }
-public static int CountAllStars(params int[] galaxies){
-	int totalStars = 0;
-	foreach(int stars in galaxies) {
-		totalStars = stars; // fix me!
-	} 
-	return totalStars;
-}
-//{ autofold
-	}
+    public class UniverseStub
+    {
+        // }
+        public static int CountAllStars(params int[] galaxies)
+        {
+            int totalStars = 0;
+            foreach (int stars in galaxies)
+            {
+                totalStars = stars; // fix me!
+            }
+            return totalStars;
+        }
+        //{ autofold
+    }
 }
 //}
